8d68c2f5ca1f44785c0ab07144f402a54a689f9f
Rutvik Pawale <rutwik268@gmail.com>
2024-02-04 22:26:04 +0530

    Update ServerMain.java

--- embeddedjetty12/src/main/java/com/embeddedjetty12/ServerMain.java
+++ embeddedjetty12/src/main/java/com/embeddedjetty12/ServerMain.java
@@ -5,6 +5,7 @@ import org.eclipse.jetty.ee10.servlet.ServletHandler;
 import org.eclipse.jetty.server.Server;
 
 public class ServerMain {
+	
 	public static void main(String[] args) throws Exception {
 		
 		Server server = new Server(8681); 
